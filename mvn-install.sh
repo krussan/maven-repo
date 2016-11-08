c1cc4b057087a83ef6464633d57dc8ff8338fa34
Christian Thunström <christian@qxx.se>
2016-11-08 10:28:00 +0100

    Added script for commiting binaries to local maven repo

---
+++ mvn-install.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+REPOSITORY=$GIT_BASE/maven-repo
+POMFILE=$1
+JARFILE=$2
+
+echo $POMFILE
+echo $JARFILE
+
+GROUPID=`sed -e "s/xmlns/ignore/" $POMFILE  | xmllint --xpath '/project/groupId/text()' -`
+ARTIFACTID=`sed -e "s/xmlns/ignore/" $POMFILE  | xmllint --xpath '/project/artifactId/text()' -`
+VERSION=`echo $JARFILE | sed 's/.jar//' | tail -c 6`
+PACKAGING=`sed -e "s/xmlns/ignore/" $POMFILE  | xmllint --xpath '/project/packaging/text()' -`
+
+echo $GROUPID
+echo $ARTIFACTID
+echo $VERSION
+echo $PACKAGING
+
+
+mvn install:install-file -Dfile=$JARFILE -DgroupId=$GROUPID -DartifactId=$ARTIFACTID -Dversion=$VERSION -Dpackaging=$PACKAGING -DlocalRepositoryPath=$REPOSITORY
+
+
+MVNPATH=`echo $GROUPID | sed -e "s/\./\//g"`
+MVNPATH=$MVNPATH/$ARTIFACTID
+
+echo $MVNPATH
+
+mv $REPOSITORY/$MVNPATH/maven-metadata-local.xml $REPOSITORY/$MVNPATH/maven-metadata.xml
